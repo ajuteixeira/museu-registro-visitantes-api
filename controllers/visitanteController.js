06895712084510b78fe70c3e71c690df0333886c
ajuteixeira <teixeirajuuh@gmail.com>
2024-02-04 22:23:39 -0300

    ajustes nos dados de data, idade e gênero

--- controllers/visitanteController.js
+++ controllers/visitanteController.js
@@ -9,7 +9,10 @@ async function listar(coluna = "visitante_id", ordem = "ASC") {
 
 async function cadastrar(data) {
   try {
-    const date = Date.now();
+    const date = new Date().toISOString().split("T")[0]; //formata a data
+
+    data.visitante_idade = parseInt(data.visitante_idade); //converte de string p/ número
+    data.visitante_genero = data.visitante_genero === "masculino" ? 1 : 2; //atribui valor 1 para masculino e 2 para feminino
 
     const linha = await DB.execute(
       `INSERT INTO ${tabela} (visitante_nome, visitante_idade, visitante_profissao, visitante_cidade, visitante_bairro, visitante_genero, visitante_cpf, visitante_data) VALUES ('${data.visitante_nome}', '${data.visitante_idade}', '${data.visitante_profissao}', '${data.visitante_cidade}', '${data.visitante_bairro}', '${data.visitante_genero}', '${data.visitante_cpf}', '${date}');`
