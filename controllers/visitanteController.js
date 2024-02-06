a015241179f25357e6120e99bfd89c7a011eb516
ajuteixeira <teixeirajuuh@gmail.com>
2024-02-06 17:39:32 -0300

    ajuste nas mensagens

--- controllers/visitanteController.js
+++ controllers/visitanteController.js
@@ -20,12 +20,9 @@ async function register(data) {
       `INSERT INTO ${tabela} (visitante_nome, visitante_idade, visitante_profissao, visitante_cidade, visitante_bairro, visitante_genero, visitante_cpf, visitante_data) VALUES ('${data.visitante_nome}', '${data.visitante_idade}', '${data.visitante_profissao}', '${data.visitante_cidade}', '${data.visitante_bairro}', '${data.visitante_genero}', '${data.visitante_cpf}', '${date}');`
     );
 
-    return "mensagem legal";
+    return "Cadastro concluído. Aproveite sua visita :)";
   } catch (error) {
-    return {
-      type: "error",
-      message: `Erro ao inserir dados: ${error.message}`,
-    };
+    return "Erro ao inserir dados";
   }
 }
 
