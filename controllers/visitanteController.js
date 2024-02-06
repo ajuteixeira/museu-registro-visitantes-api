201874681f03f58615110a136384ea5f09968f87
ajuteixeira <teixeirajuuh@gmail.com>
2024-02-06 13:38:52 -0300

    ajustes nas formatações e exibições dos dados de gênero e data

--- controllers/visitanteController.js
+++ controllers/visitanteController.js
@@ -2,9 +2,11 @@ const DB = require("../database/index");
 const tabela = "visitante";
 
 async function listAll(coluna = "visitante_id", ordem = "ASC") {
-  return await DB.execute(
+  const results = await DB.execute(
     `SELECT * FROM ${tabela} ORDER BY ${coluna} ${ordem};`
   );
+
+  return results.map((record) => parseRecord(record));
 }
 
 async function register(data) {
@@ -25,6 +27,56 @@ async function register(data) {
       message: `Erro ao inserir dados: ${error.message}`,
     };
   }
+}
+
+function parseRecord(record) {
+  const parsedRecord = {};
+
+  Object.keys(record).forEach((field) => {
+    parsedRecord[field] = parseFieldValue(field, record[field]);
+  });
+
+  return parsedRecord;
+}
+
+function parseFieldValue(key, value) {
+  switch (key) {
+    case "visitante_genero":
+      return formatGenre(value);
+      break;
+    case "visitante_data":
+      return formatDate(value);
+      break;
+    default:
+      return value;
+  }
+}
+
+//formata o gênero para exibição no gráfico
+function formatGenre(value) {
+  return value === 1 ? "Masculino" : "Feminino";
+}
+
+//seleciona apenas o mês da data e escreve por extenso
+function formatDate(value) {
+  const date = new Date(value);
+  const month = date.getMonth();
+  const monthNames = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
+
+  return monthNames[month];
 }
 
 module.exports = {
